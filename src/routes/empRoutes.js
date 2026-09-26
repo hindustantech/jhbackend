f9e20b9f8c2019569715af8af051110c8c109b6e
hindustantech <kumarrupees505@gmail.com>
2026-09-26 11:09:43 +0530

    aadd

--- src/routes/empRoutes.js
+++ src/routes/empRoutes.js
@@ -6,7 +6,7 @@ import { authorize } from "../middleware/authorizationMiddleware.js";
 const router = express.Router();
 
 router.post("/", protect, authorize("employees:manage"), createEmp);
-router.get("/", protect, authorize("employees:manage"), getAllEmps);
+router.get("/", getAllEmps);
 router.get("/:id", protect, authorize("employees:manage"), getEmpById);
 router.put("/:id", protect, authorize("employees:manage"), updateEmp);
 router.delete("/:id", protect, authorize("employees:manage"), deleteEmp);
